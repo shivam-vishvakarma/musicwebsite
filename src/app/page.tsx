4edf1f1bd91e7ab4a813bac8020eed005a9b1b00
Shivam VishWakarma <shivamvishva552@gmail.com>
2024-02-24 20:47:17 +0530

    added herosection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HeroSection from "@/Components/HeroSection";
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased">
-      <h1 className="text-2xl text-center">Musicaly</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
     </main>
   );
 }
