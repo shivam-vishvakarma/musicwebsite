5cda8a3c79a358f1761580095a31e642f317106a
Shivam VishWakarma <shivamvishva552@gmail.com>
2024-02-25 00:00:28 +0530

    added why choose us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import FeaturedCourses from "@/Components/FeaturedCourses";
 import HeroSection from "@/Components/HeroSection";
+import WhyChooseUs from "@/Components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
     </main>
   );
 }
