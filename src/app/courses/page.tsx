369a39f73f341ee3f028922f4a617e6e2ecdf2e9
Shivam Vishvakarma <shivamvishva552@gmail.com>
2024-03-02 23:11:40 +0530

    code improvement

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -7,13 +7,13 @@ export default function Page() {
   const courses = data.courses;
   return (
     <main className="min-h-screen bg-black py-12 pt-36">
-      <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">
+      <h1 className="text-lg md:text-5xl text-center font-sans font-bold mb-8 text-white">
         All courses ({courses.length})
       </h1>
       <div className="flex flex-wrap justify-center">
         {courses.map((course) => (
-          <CardContainer className="inter-var m-4" key={course.id}>
-            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+          <CardContainer className="inter-var mx-2" key={course.id}>
+            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto min-w-min sm:w-[25rem] h-auto rounded-xl p-5 border">
               <CardItem
                 translateZ="50"
                 className="text-xl font-bold text-neutral-600 dark:text-white"
@@ -27,7 +27,7 @@ export default function Page() {
               >
                 {course.description}
               </CardItem>
-              <CardItem translateZ="100" className="w-full mt-4">
+              <CardItem translateZ="100" className="w-full mt-3">
                 <Image
                   src={course.image}
                   height="1000"
@@ -36,7 +36,7 @@ export default function Page() {
                   alt={course.title}
                 />
               </CardItem>
-              <div className="flex justify-between items-center mt-20">
+              <div className="flex justify-between items-center mt-10">
                 <CardItem
                   translateZ={20}
                   as="button"
